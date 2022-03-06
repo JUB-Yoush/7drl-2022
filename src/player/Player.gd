97b5a34e4b4ce2c2bd0657ad2e25280bcd1ded61
JUB_Yoush <yoush1337@gmail.com>
2022-03-05 19:56:32 -0500

    started making player scene, fake grids!

---
+++ src/player/Player.gd
@@ -0,0 +1,10 @@
+extends Area2D
+# everything is using a fake grid indivisually generated
+
+var tile_size:int = 16
+
+
+func _input(event):
+	if Input.is_action_just_pressed("ui_up"):
+		pass
+	pass
